495143b808e74dc7a054f41ac897c3fa1666f42c
John Wiegley <johnw@newartisans.com>
2014-07-19 22:34:26 -0500

    More exercises

--- src/List.hs
+++ src/List.hs
@@ -4,49 +4,52 @@ import Control.Applicative
 import Control.Monad
 import Test.QuickCheck
 
-newtype List a = List { unList :: forall r. r -> (r -> a -> r) -> r }
+newtype List a = List { unList :: forall r. r -> (a -> r -> r) -> r }
+
+-- newtype List a = List { unList :: forall r. Cont (r -> r) a }
 
 instance Show a => Show (List a) where
     show (List h) =
-        "[" ++ h "" (\r a -> if null r then show a else show a ++ ", " ++ r)
-            ++ "]"
+        "[" ++ h "" (\a r -> if null r
+                             then show a
+                             else show a ++ ", " ++ r) ++ "]"
 
 toList :: List a -> [a]
-toList (List h) = h [] (flip (:))
+toList (List h) = h [] (:)
 
 fromList :: [a] -> List a
-fromList xs = List $ \r f -> foldr (flip f) r xs
+fromList xs = List $ \r f -> foldr f r xs
 
 instance Eq a => Eq (List a) where
     x == y = toList x == toList y
 
 instance Functor List where
-    fmap f (List h) = List $ \r k -> h r (\s a -> k s (f a))
+    fmap f (List h) = List $ \r k -> h r (k . f)
 
 instance Applicative List where
-    pure x = List $ \r k -> k r x
-    List f <*> List x = List $ \r k -> f r $ \s h -> x s (\t -> k t . h)
+    pure x = List $ \r k -> k x r
+    List f <*> List x = List $ \r k -> f r $ \h s -> x s (k . h)
 
 instance Monad List where
     return = pure
-    List h >>= f = List $ \r k -> h r (\s a -> unList (f a) s k)
+    List h >>= f = List $ \r k -> h r (\a s -> unList (f a) s k)
 
 -- Relies on the laziness of foldr (in fromList) to work properly
 head :: List a -> a
-head (List h) = h (error "Empty list!") (curry snd)
+head (List h) = h (error "Empty list!") const
 
 split :: List a -> List (a, List a)
 split (List h) = h (List const) ssk
   where
-    ssk fk v = List $ \r f ->
-        f r (v, List $ \s k -> unList fk s (\_ (v', x) -> k (unList x s k) v'))
+    ssk v fk = List $ \r f ->
+        f (v, List $ \s k -> unList fk s (\(v', x) _ -> k v' (unList x s k))) r
 
 tail :: List a -> List a
 tail = join . fmap snd . split
 
 init :: List a -> List a
-init (List h) = join $ h (List const) $ \fk v -> List $ \r f ->
-    f r $ List $ \s k -> unList fk s $ \_ x -> k (unList x s k) v
+init (List h) = join $ h (List const) $ \v fk -> List $ \r f ->
+    f (List $ \s k -> unList fk s $ \x _ -> k v (unList x s k)) r
 
 instance Arbitrary a => Arbitrary (List a) where
     arbitrary = fmap fromList arbitrary
